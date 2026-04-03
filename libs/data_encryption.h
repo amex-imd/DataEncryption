09e44c7a41387eacb9593b06a4db48de0e10cdbe
amex-imd <ameximd@gmail.com>
2026-04-03 16:53:29 +0300

    The block 'XOR ENCRYPTION' was realized

--- libs/data_encryption.h
+++ libs/data_encryption.h
@@ -3,12 +3,32 @@
 
 #include <string>
 #include <cstddef>
-
+#include <stdexcept>
 namespace IMD
 {
+    // -------- CAESAR CIPHER--------
     std::string Caesar_cipher_encrypt(const std::string &line, size_t shift, const std::string &alphabet);
     std::string Caesar_cipher_decrypt(const std::string &line, size_t shift, const std::string &alphabet);
 
+    // --------XOR ENCRYPTION--------
+
+    template <typename Container>
+    Container XOR_transform(const Container &data, const Container &key)
+    {
+        size_t N(data.size());
+        size_t L(key.size());
+
+        if (N != L)
+            throw std::runtime_error("The arguments 'data' and 'key' must have the same size");
+
+        Container res;
+        res.reserve(N);
+
+        for (size_t i(0); i < N; ++i)
+            res.push_back(data[i] ^ key[i]);
+
+        return res;
+    }
 }
 
 #endif
--- src/program.cpp
+++ src/program.cpp
@@ -4,15 +4,16 @@
 
 int main()
 {
-    std::string line = "HelloWorld";
-    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    size_t shift = 4;
+    std::string line = "Hello";
+    std::string key = "12345";
 
-    std::cout << "Source line: " << line << std::endl;
-    std::string enc = IMD::Caesar_cipher_encrypt(line, shift, alphabet);
-    std::cout << "Encrypted line: " << enc << std::endl;
-    std::string dec = IMD::Caesar_cipher_decrypt(enc, shift, alphabet);
-    std::cout << "Decrypted line: " << dec << std::endl;
+    auto code = IMD::XOR_encrypt(line, key);
+
+    std::cout << "Size: " << code.size() << " Code: " << code << std::endl;
+
+    code = IMD::XOR_encrypt(code, key);
+
+    std::cout << "Size: " << code.size() << " Code: " << code << std::endl;
 
     std::cin.get();
 
